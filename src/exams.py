98e134f837e361429e34d61f094ef8d0d6974ce3
blexiestar <blessedkijambu@gmail.com>
2025-02-01 10:17:42 +0300

    FEATURE: I added question 16

--- src/exams.py
+++ src/exams.py
@@ -1,22 +1,28 @@
 # qn 18
-email_address = input("put ur email_address: ")
- print(email_address)
- user_name = email_address.partition("@")[0]
- print(user_name)
- domain_name = email_address.partition("@")[-1]
- print(domain_name)
+# email_address = input("put ur email_address: ")
+#  print(email_address)
+#  user_name = email_address.partition("@")[0]
+#  print(user_name)
+#  domain_name = email_address.partition("@")[-1]
+#  print(domain_name)
 
- #qn 17
-customer = input("put ur name: ")
-tickets = input("how many tickets?: ")
-charge = 12
-total_cost = tickets * charge
-print(f"dear {customer} you have purchsed {tickets} tickets costs $ {total_cost}")
+#  #qn 17
+# customer = input("put ur name: ")
+# tickets = input("how many tickets?: ")
+# charge = 12
+# total_cost = tickets * charge
+# print(f"dear {customer} you have purchsed {tickets} tickets costs $ {total_cost}")
 
-# qn 19
-statement = input("put ur statement: ")
-print(statement.split())
-words = statement.split()
-hypened = "-".join(words)
-print(hypened)
+# # qn 19
+# statement = input("put ur statement: ")
+# print(statement.split())
+# words = statement.split()
+# hypened = "-".join(words)
+# print(hypened)
+
+#qn 16
+full_name = input("Enter your full name: ")
+fav_number = input("Enter your favorite number: ")
+handle = full_name[:4] + fav_number + "traac"
+print(handle)
 
