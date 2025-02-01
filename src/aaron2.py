f2a511a7d5e9bd7f340f6ba1eec02ba98f4fd4b0
aaroncar <akampaaaron82@gmail.com>
2025-02-01 10:16:47 +0300

    TEATURE:i have added a src folder

---
+++ src/aaron2.py
@@ -0,0 +1,9 @@
+print ("welcome to my cinema booking system")
+name =input("enter your name")
+tickets =int(input("how many tickets would you like to book"))
+ticket_price =12
+total_cost =ticket*ticket_price
+print(f"customersname:{name"})
+print(f"numbero of tickets:{tickets"})
+print( "thank you for booking")
+
