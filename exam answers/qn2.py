558e0c2f273f8d0da41c5046f3788c9921cb5759
khangu1 <khangumelissa@gmail.com>
2025-02-01 10:36:28 +0300

    Khangu Melissa has added new files

---
+++ exam answers/qn2.py
@@ -0,0 +1,4 @@
+name=input("what's your name")
+fav_number=input("what is your fav number")
+handle=name+fav_number+"traac"
+print(handle.lower())
---
+++ exam answers/qn4.py
@@ -0,0 +1,4 @@
+sentence = input("Enter a sentence: ")
+sentnce_formated = sentence.split()
+output = '-'.join(sentnce_formated)
+print(output)
